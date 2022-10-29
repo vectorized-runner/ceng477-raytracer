a0dfaa626b3096605ec6fd2686839887c2d7dcfb
Bartu Baş <bartubas99@gmail.com>
2022-10-29 13:42:19 +0300

    Add Intersection result

---
+++ src/Data/Collision/IntersectionResult.h
@@ -0,0 +1,14 @@
+
+#ifndef CENG477_RAYTRACER_INTERSECTIONRESULT_H
+#define CENG477_RAYTRACER_INTERSECTIONRESULT_H
+
+namespace RayTracer {
+
+    struct IntersectionResult {
+        ObjectId ObjectId;
+        float Distance;
+    };
+
+} // RayTracer
+
+#endif //CENG477_RAYTRACER_INTERSECTIONRESULT_H
