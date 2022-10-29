b8acb1d439aac9218489d88f0daa16b0c310fcb8
Bartu Baş <bartubas99@gmail.com>
2022-10-29 14:56:31 +0300

    MeshData

---
+++ src/Data/Objects/MeshData.h
@@ -0,0 +1,15 @@
+
+#ifndef CENG477_RAYTRACER_MESHDATA_H
+#define CENG477_RAYTRACER_MESHDATA_H
+
+#include "Mesh.h"
+
+namespace RayTracer {
+
+    struct MeshData {
+        Mesh* Meshes;
+    };
+
+} // RayTracer
+
+#endif //CENG477_RAYTRACER_MESHDATA_H
