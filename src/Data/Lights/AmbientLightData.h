db4dc3416af79b4b2f93fc20408b73c74ea8a637
Bartu Baş <bartubas99@gmail.com>
2022-10-29 13:53:28 +0300

    AmbientLightData

---
+++ src/Data/Lights/AmbientLightData.h
@@ -0,0 +1,15 @@
+
+#ifndef CENG477_RAYTRACER_AMBIENTLIGHTDATA_H
+#define CENG477_RAYTRACER_AMBIENTLIGHTDATA_H
+
+#include "../../float3.h"
+
+namespace RayTracer {
+
+    struct AmbientLightData {
+        float3 Radiance;
+    };
+
+} // RayTracer
+
+#endif //CENG477_RAYTRACER_AMBIENTLIGHTDATA_H
