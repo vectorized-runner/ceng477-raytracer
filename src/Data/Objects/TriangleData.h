f27829355b670a4021727612bfe66d4012085880
Bartu Baş <bartubas99@gmail.com>
2022-10-29 14:54:27 +0300

    TriangleData

---
+++ src/Data/Objects/TriangleData.h
@@ -0,0 +1,18 @@
+
+#ifndef CENG477_RAYTRACER_TRIANGLEDATA_H
+#define CENG477_RAYTRACER_TRIANGLEDATA_H
+
+#include "Triangle.h"
+#include "../Shading/MaterialData.h"
+
+namespace RayTracer {
+
+    struct TriangleData {
+        Triangle* Triangles;
+        float3* Normals;
+        MaterialData* Materials;
+
+
+    }; // RayTracer
+}
+#endif //CENG477_RAYTRACER_TRIANGLEDATA_H
