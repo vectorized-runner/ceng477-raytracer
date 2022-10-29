e9109d505293e01850e4e0fa7fa720b987a7b018
Bartu Baş <bartubas99@gmail.com>
2022-10-29 14:52:55 +0300

    SphereData

---
+++ src/Data/Objects/SphereData.h
@@ -0,0 +1,18 @@
+
+#ifndef CENG477_RAYTRACER_SPHEREDATA_H
+#define CENG477_RAYTRACER_SPHEREDATA_H
+
+#include "Sphere.h"
+#include "../Shading/MaterialData.h"
+
+namespace RayTracer {
+
+    struct SphereData {
+        Sphere* Spheres;
+        MaterialData* Materials;
+    };
+
+
+} // RayTracer
+
+#endif //CENG477_RAYTRACER_SPHEREDATA_H
