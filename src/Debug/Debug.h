fccf9fd85d06491492e9590ca4658445d6d25c27
Bartu Baş <bartubas99@gmail.com>
2022-10-30 20:54:47 +0300

    Add Debug defines

--- src/Debug/Debug.h
+++ src/Debug/Debug.h
@@ -6,29 +6,43 @@
 
 using namespace std;
 
+#ifndef Self_Debug
+//#define Self_Debug
+#endif
+
 namespace RayTracer {
 
     struct Debug {
         static void Assert(bool condition, const string& message) {
+#ifdef Self_Debug
             if (!condition) {
                 Log("Assertion Failed: " + message);
             }
+#endif
         }
 
         static void Log(const string& message) {
+#ifdef Self_Debug
             cout << message << endl;
+#endif
         }
 
         static void Log(const string& message, int i) {
+#ifdef Self_Debug
             cout << message << i << endl;
+#endif
         }
 
         static void Log(const string& message, float f) {
+#ifdef Self_Debug
             cout << message << f << endl;
+#endif
         }
 
         static void Log(const string& message, char* c) {
+#ifdef Self_Debug
             cout << message << c << endl;
+#endif
         }
     };
 
